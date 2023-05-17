7a384abbd315b3b4aa4f0a9ad6cf236e251eacef
John-Development <38006800+John-Development@users.noreply.github.com>
2023-05-17 19:51:25 +0200

    Two equal slabs can be placed in the same block [WIP] Added tool tags

--- src/main/java/eu/midnightdust/motschen/verticalslabs/block/VerticalSlabBlock.java
+++ src/main/java/eu/midnightdust/motschen/verticalslabs/block/VerticalSlabBlock.java
@@ -1,26 +1,32 @@
 package eu.midnightdust.motschen.verticalslabs.block;
 
+import eu.midnightdust.motschen.verticalslabs.block.enums.VerticalSlabType;
 import net.minecraft.block.*;
 import net.minecraft.entity.ai.pathing.NavigationType;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.WorldAccess;
 
+import javax.annotation.Nullable;
 import java.util.Objects;
 
 
 public class VerticalSlabBlock extends HorizontalFacingBlock implements Waterloggable {
   public static final BooleanProperty WATERLOGGED;
+  public static final EnumProperty<VerticalSlabType> TYPE;
   private static final VoxelShape NORTH_SHAPE;
   private static final VoxelShape EAST_SHAPE;
   private static final VoxelShape SOUTH_SHAPE;
@@ -28,7 +34,7 @@ public class VerticalSlabBlock extends HorizontalFacingBlock implements Waterlog
 
   public VerticalSlabBlock(Settings settings) {
     super(settings);
-    this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH).with(WATERLOGGED, true));
+    this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH).with(WATERLOGGED, true).with(TYPE, VerticalSlabType.HALF));
   }
 
   @Override
@@ -67,21 +73,16 @@ public class VerticalSlabBlock extends HorizontalFacingBlock implements Waterlog
   }
 
   @Override
-  public BlockState getPlacementState(ItemPlacementContext itemPlacementContext) {
-    FluidState fluidState = itemPlacementContext.getWorld().getFluidState(itemPlacementContext.getBlockPos());
-    boolean waterLog = fluidState.isIn(FluidTags.WATER) && fluidState.getLevel() == 8;
-
-    return Objects.requireNonNull(super.getPlacementState(itemPlacementContext)).with(WATERLOGGED, waterLog)
-      .with(FACING, itemPlacementContext.getHorizontalPlayerFacing().getOpposite());
-  }
-
-  @Override
   protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-    builder.add(WATERLOGGED, FACING);
+    builder.add(WATERLOGGED, FACING, TYPE);
   }
 
   @Override
   public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+    if (state.get(TYPE) == VerticalSlabType.DOUBLE) {
+      return VoxelShapes.fullCube();
+    }
+
     return switch (state.get(FACING)) {
       case NORTH -> NORTH_SHAPE;
       case EAST -> EAST_SHAPE;
@@ -91,8 +92,50 @@ public class VerticalSlabBlock extends HorizontalFacingBlock implements Waterlog
     };
   }
 
+  @Nullable
+  public BlockState getPlacementState(ItemPlacementContext ctx) {
+    BlockPos blockPos = ctx.getBlockPos();
+    BlockState blockState = ctx.getWorld().getBlockState(blockPos);
+    if (blockState.isOf(this)) {
+      return (blockState.with(TYPE, VerticalSlabType.DOUBLE)).with(WATERLOGGED, false);
+    } else {
+      FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
+      boolean waterLog = fluidState.isIn(FluidTags.WATER) && fluidState.getLevel() == 8;
+
+      return Objects.requireNonNull(super.getPlacementState(ctx)).with(WATERLOGGED, waterLog)
+        .with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+    }
+  }
+
+  public boolean canReplace(BlockState state, ItemPlacementContext context) {
+    ItemStack itemStack = context.getStack();
+    VerticalSlabType verticalSlabType = state.get(TYPE);
+    Direction facing = state.get(FACING);
+
+    if (verticalSlabType != VerticalSlabType.DOUBLE && itemStack.isOf(this.asItem())) {
+      if (context.canReplaceExisting()) {
+        boolean bl = context.getHitPos().y - (double)context.getBlockPos().getY() > 0.5; // true si pincha mas arriba false si pincha mas abajo
+        boolean blSouth = context.getHitPos().z - (double)context.getBlockPos().getZ() > 0.5; // true si pincha mas sur false si pincha mas norte
+        boolean blEast = context.getHitPos().x - (double)context.getBlockPos().getX() > 0.5; // true si pincha mas este false si pincha mas oeste
+        Direction direction = context.getSide();
+        return switch (facing) {
+          case NORTH -> direction == Direction.SOUTH || blSouth && direction.getAxis().isHorizontal();
+          case SOUTH -> direction == Direction.NORTH || !blSouth && direction.getAxis().isHorizontal();
+          case EAST -> direction == Direction.WEST || blEast && direction.getAxis().isHorizontal();
+          case WEST -> direction == Direction.EAST || !blEast && direction.getAxis().isHorizontal();
+          default -> false;
+        };
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+
   static {
     WATERLOGGED = Properties.WATERLOGGED;
+    TYPE = EnumProperty.of("type", VerticalSlabType.class);
     NORTH_SHAPE = createCuboidShape(0, 0, 8, 16, 16, 16);
     EAST_SHAPE = createCuboidShape(0, 0, 0, 8, 16, 16);
     SOUTH_SHAPE = createCuboidShape(0, 0, 0, 16, 16, 8);
